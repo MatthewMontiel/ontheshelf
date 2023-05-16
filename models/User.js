70cdb9b560cfabe16cc21ba904c9e6fb6b2f4323
LindseyIsaac <isaaclindsey89@gmail.com>
2023-05-15 19:46:54 -0500

    Jackie provided all info

--- models/User.js
+++ models/User.js
@@ -59,3 +59,73 @@ User.init(
 );
 
 module.exports = User;
+
+// const { Model, DataTypes } = require("sequelize");
+// const bcrypt = require("bcrypt");
+// const sequelize = require("../config/connection");
+// const { v4: uuidv4 } = require("uuid");
+
+
+// class User extends Model {
+//  checkPassword(passwordInput) {
+//    return bcrypt.compareSync(passwordInput, this.password);
+//  }
+// }
+
+
+// // USE UUID WITH PRIMARY KEY?
+
+
+// User.init(
+//  {
+//    id: {
+//      type: DataTypes.INTEGER,
+//      autoIncrement: true,
+//      allowNull: false,
+//      primaryKey: true,
+//    },
+//    name: {
+//      type: DataTypes.STRING,
+//      allowNull: false,
+//    },
+//    email: {
+//      type: DataTypes.STRING,
+//      allowNull: false,
+//      unique: true,
+//      validate: {
+//        isEmail: true,
+//      },
+//    },
+//    password: {
+//      type: DataTypes.STRING,
+//      allowNull: false,
+//      validate: {
+//        len: [8, 23],
+//      },
+//    },
+//  },
+//  {
+//    // VINCENT DIG INTO BCRYPT
+//    hooks: {
+//      beforeCreate: async (newUserData) => {
+//        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+//        return newUserData;
+//      },
+//      beforeUpdate: async (updatedUserData) => {
+//        updatedUserData.password = await bcrypt.hash(
+//          updatedUserData.password,
+//          10
+//        );
+//        return updatedUserData;
+//      },
+//    },
+//    sequelize,
+//    timestamps: false,
+//    freezeTableName: true,
+//    underscored: true,
+//    modelName: "user",
+//  }
+// );
+
+
+// module.exports = User;
