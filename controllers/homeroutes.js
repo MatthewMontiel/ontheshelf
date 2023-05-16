25b123b5a61c9e16848785efcdf5ee2230d34762
dctristero <123331986+dctristero@users.noreply.github.com>
2023-05-16 15:57:38 -0500

    great triumph

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,8 +1,48 @@
+const router = require("express").Router();
+const authorize = require('../utils/authorize');
 
-const router = require("express").Router()
-router.get("/", (req,res) => {
-   res.render("homepage")
-})
+router.get("/", (req, res) => {
+  res.render("homepage");
+});
 
+router.get("/", async (req, res) => {
+  try {
+    const gameData = await Game.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
 
-module.exports = router
+    const games = gameData.map((game) => game.get({ plain: true }));
+
+    res.render("homepage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/shelf', authorize, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Game }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('shelf', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// logged_in: req.session.logged_in WITHIN RES RENDER?
+
+module.exports = router;
